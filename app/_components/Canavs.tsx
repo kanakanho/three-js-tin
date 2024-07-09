14867d88c98209c24bc1435a487a164e4199a99d
kanakanho <kahoshibakichi@gmail.com>
2024-07-09 18:26:25 +0900

    fix: wireframeにラップを解除

--- app/_components/Canavs.tsx
+++ app/_components/Canavs.tsx
@@ -3,34 +3,26 @@ import { XR } from "@react-three/xr";
 import type React from "react";
 import { useEffect, useState } from "react";
 import type { Vector3 } from "three";
-import { TriangleComponent } from "./Triangle";
+import type THREE from "three";
+import { BufferGeometry, DoubleSide } from "three";
 
 type Props = {
   points: Vector3[];
 };
 
 export const CanvasComponent: React.FC<Props> = ({ points }) => {
-  const [polygonVertices, setPolygonVertices] = useState<Vector3[][]>([]);
+  const [geometry, setGeometry] = useState<THREE.BufferGeometry>();
 
   useEffect(() => {
-    setPolygonVertices([points]);
-
-    // 三角形の頂点を設定
-    const triangleVertices = [];
-    for (let i = 0; i < points.length; i += 3) {
-      triangleVertices.push([points[i], points[i + 1], points[i + 2]]);
-    }
+    setGeometry(new BufferGeometry().setFromPoints(points));
   }, [points]);
 
   return (
-    <>
-      <XR>
-        <ambientLight />
-        {polygonVertices.map((vertices, index) => (
-          // biome-ignore lint/suspicious/noArrayIndexKey: <explanation>
-          <TriangleComponent key={index} vertices={vertices} />
-        ))}
-      </XR>
-    </>
+    <XR>
+      <ambientLight />
+      <mesh geometry={geometry}>
+        <meshBasicMaterial color={"black"} wireframe={true} side={DoubleSide} />
+      </mesh>
+    </XR>
   );
 };
